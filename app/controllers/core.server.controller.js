478bc472c6e34087ea7a3436fd4ecd97e511399e
Trey Hunner <trey@treyhunner.com>
2014-05-18 23:52:00 -0700

    Add JSON search controller

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -3,8 +3,41 @@
 /**
  * Module dependencies.
  */
+var mongoose = require('mongoose'),
+	JamendoArtist = mongoose.model('JamendoArtist'),
+	MagnatuneArtist = mongoose.model('MagnatuneArtist'),
+	async = require('async'),
+	_ = require('lodash');
+
+	/**
+	* Homepage
+	*/
 exports.index = function(req, res) {
 	res.render('index', {
 		user: req.user || null
+	});
+};
+
+/**
+ * Search for artist by name
+ */
+exports.search = function(req, res) {
+	var query = req.query.q;
+	async.parallel([
+		function (callback) {
+			MagnatuneArtist.find({artist: query}, callback);
+		},
+		function (callback) {
+			JamendoArtist.find({name: query}, callback);
+		}
+	],
+	function (err, results) {
+		if (err) {
+			res.send(400, {
+				message: 'error',  // FIXME
+			});
+		} else {
+			res.jsonp(_.flatten(results, true));
+		}
 	});
 };
